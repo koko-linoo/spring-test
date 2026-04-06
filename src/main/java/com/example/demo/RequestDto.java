5d14fdd1356b269fdecf4ccbc722e745f3c86947
Ko Ko Lin Oo <ko.kolinoo@xyphatech.com>
2026-04-06 08:42:55 +0630

    example fix

--- src/main/java/com/example/demo/RequestDto.java
+++ src/main/java/com/example/demo/RequestDto.java
@@ -9,12 +9,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RequestDto {
-  @Schema(example = "{\n" + //
-      "  \"users\": [\n" + //
-      "    { \"id\": 1, \"name\": \"John Doe\", \"email\": \"john@example.com\" },\n" + //
-      "    { \"id\": 2, \"name\": \"Bob Smith\", \"email\": \"bob_new@example.com\" },\n" + //
-      "    { \"name\": \"New User\", \"email\": \"new@example.com\" }\n" + //
-      "  ]\n" + //
-      "}", description = "Users")
+  @Schema(description = "Users", example = "[\n" +
+      "  { \"id\": 1, \"name\": \"John Doe\", \"email\": \"john@example.com\" },\n" +
+      "  { \"id\": 2, \"name\": \"Bob Smith\", \"email\": \"bob_new@example.com\" },\n" +
+      "  { \"name\": \"New User\", \"email\": \"new@example.com\" }\n" +
+      "]")
   private List<User> users;
 }
